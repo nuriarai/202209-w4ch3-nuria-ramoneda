c772f3c3bad6b43a411e5d4276bf63f8ad5ed02a
Núria Ramoneda <nuriarai@gmail.com>
2022-10-19 19:09:36 +0200

    Create key component

---
+++ src/components/Key/Key.tsx
@@ -0,0 +1,11 @@
+interface KeyProps {
+  text: string;
+  className: string;
+  action: () => void;
+}
+
+const Key = ({ text, className, action }: KeyProps): JSX.Element => {
+  return <button className={className}>{text}</button>;
+};
+
+export default Key;
